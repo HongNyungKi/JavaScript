2b5af43caefd88d47e2416e28d5eb09d2086f794
HongNyungKi <hnk2017@naver.com>
2020-04-24 19:25:40 +0900

    [js clock]

---
+++ userName.js
@@ -0,0 +1,10 @@
+// querySelctor에는 IDBCursor,class,tag 다 지정할 수 있고 찾은 첫번째 것을 가져온다.
+// querySelectorAll은 찾은 모든걸 가져와서 배열(array를 준다.)
+
+const form = documnet.querySelector(".js-form"),
+    input = form.querySelector("input");
+
+function init() {
+
+}
+init();
