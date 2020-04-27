12e9778e7bb2fbc24b9b23fad0c0997805814712
HongNyungKi <hnk2017@naver.com>
2020-04-27 20:10:29 +0900

    [Add]~userName

--- clock.js
+++ clock.js
@@ -1,14 +1,5 @@
-// document의 자식을 탐구하고 싶을때 document.querySelector 사용
-// const clockContainer = document.querySelector(".js-clock");
-// clockContainer의 자식을 탐구하고 싶을때는 밑에처럼. 
-// 단, 반드시 class명이 아닌 태그를 지정하여도 된다.
-// const clockTitle = clockContainer.querySelector("h1");
-
-// 문자에 변수를 삽입하기 위해서는 `` 와 $ 기호를 쓴다.
-
-// 두개의 const를 간단히 쓰려면
-const clockContainer = document.querySelector(".js-clock");
-const clockTitle = clockContainer.querySelector("h1");
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
     const date = new Date();
@@ -18,8 +9,6 @@ function getTime() {
     clockTitle.innerText = `${hours<10 ? `0${hours}` : hours}:${minutes<10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
-// 기본적인 함수틀이다.
-// 위에 함수를 작성하고 이 기본함수로 부터 불러들인다.
 function init() {
     getTime();
     setInterval(getTime, 1000);
--- userName.js
+++ userName.js
@@ -2,9 +2,28 @@
 // querySelectorAll은 찾은 모든걸 가져와서 배열(array를 준다.)
 
 const form = documnet.querySelector(".js-form"),
-    input = form.querySelector("input");
+    input = form.querySelector("input"),
+    greeting = document.querySelector("js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+
+    } else {
+        paintGreeting(currentUser);
+    }
+}
 
 function init() {
-
+    loadName()
 }
 init();
