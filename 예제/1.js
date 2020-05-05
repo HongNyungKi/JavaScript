1d2eb0b94449b2e2719e33115716b969137ccdfc
HongNyungKi <hnk2017@naver.com>
2020-05-05 19:14:16 +0900

    [Add]

--- 예제/1.js
+++ 예제/1.js
@@ -1,26 +1,12 @@
-//삼항연산자
+const number = document.getElementById('number');
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
 
-const array = [];
-let text = '';
-if(array.length === 0){
-text = '배열이 비어있습니다.';
-}else {
-text='배열이 비어있지 않습니다.';
+increase.onclick = () => {
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current + 1;
 }
-console.log(text);
-// 배열이 비어있습니다.
-
-const array = [1];
-let text = '';
-if(array.length === 0){
-text = '배열이 비어있습니다.';
-}else {
-text='배열이 비어있지 않습니다.';
+decrease.onclick = () => {
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current - 1;
 }
-console.log(text);
-//배열이 비어있지 않습니다.
-
-const array = [];
-let text = array.length === 0 ? '배열이 비어있습니다.' : '배열이 비어있지 않습니다.';
-console.log(text);
-// 배열이 비어 있습니다.
--- 예제/2.js
+++ 예제/2.js
@@ -1,29 +1,11 @@
-//Truthy 와 Falsy
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.querySelector('.modal-wrapper');
 
-//Null Checking
-function print(person){
-if(person===undefined||person===null){
-    return ;
-}
-console.log(person.name);
-}
+open.onclick = () => {
+    modal.style.display = "flex";
+};
 
-const person = {
-    name:'Hong'
-}
-
-print(person);
-
-//상위의 null checking함수를 더욱 간단하게 하기 위해
-function print(person){
-if(!person){
-    return;
-}
-console.log(person.name);
-}
-
-const person = {name:'hong'}
-
-print(person);
-//만약, person이 null이나 undefined와 같이 비어 있는 값일 경우 if문의 조건이 참이 되어 아무것도 안하는 return을 하게되고, 
-//person이 어떠한 값이 라도 있는경우, if문은 실행되지 않고 바로 console.log가 실행되는 원리이다. 
+close.onclick = () => {
+    modal.style.display = "none";
+};
